7446f294a39044c8a8c3c4abdc14c33174bf7b1c
BaileyBruketta <49525642+BaileyBruketta@users.noreply.github.com>
2020-06-24 19:02:09 -0500

    Ebay Api Call and search for lowest buy now price

---
+++ hyo-comparison-app/README.md
@@ -0,0 +1,2 @@
+# HYO ECommerce Comparison App
+A tool for comparing item pricing across E-commerce platforms
---
+++ hyo-comparison-app/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{color: "white"}}>COMPARISON APP</h1>
+    </div>
+  );
+}
+
+export default App;
