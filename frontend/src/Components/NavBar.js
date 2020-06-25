39eccdaa5b817d306fc2004feea007ba79aa9dd7
jack <jackbisceglia2000@gmail.com>
2020-06-25 15:42:29 -0400

    Navbar created with brandname and account link

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Style Imports
-import  { Navbar } from 'react-bootstrap';
+import  { Navbar, Nav } from 'react-bootstrap';
+import '../Styles/navStyles.css';
 
-export default function NavBar() {
+export default function NavBar( {homePage} ) {
+    const [onHomePage, setOnHomePage] = useState(homePage)
+
     return (
-        <Navbar bg="dark">
-
+        <Navbar bg="dark" expand="lg" className="navbar-custom">
+            <Navbar.Brand href="#" style={{color: "white", fontWeight: "bold", fontSize: "1.2rem"}} className="nav-title">CompareIt</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="justify-content-end" style={{ width: "100%" }}>
+                    <Nav.Item>
+                        <Nav.Link className="custom-nav-item" href="/" style={{color: "#41E0FD"}}>Account</Nav.Link>                    
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
