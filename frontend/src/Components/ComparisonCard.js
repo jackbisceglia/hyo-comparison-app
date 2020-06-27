84bcd86a21ce8f3fbec308dc827511b7ee7205df
jack <jackbisceglia2000@gmail.com>
2020-06-26 20:58:16 -0400

    Layout updated/Card Updated and outline on hover

--- frontend/src/Components/ComparisonCard.js
+++ frontend/src/Components/ComparisonCard.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 // Style Imports
 import '../Styles/comparisonCard.css';
 import { Button, Card } from 'react-bootstrap';
 
 export default function ComparisonCard({ site, item, price, img}) {
+    const [isHovering, setIsHovering] = useState(false);
+
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={img} />
+        <Card 
+            className={"cardcustom pt-2 d-block h-100 ml-3 mr-3"} 
+            style={isHovering ? {border: "1px solid #3FE0FD", cursor: "pointer"} : {}}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+            appear={true}
+        >
+            <Card.Img variant="top" src={img} style={{maxWidth: '30%'}}/>
             <Card.Body>
                 <Card.Title>{site}</Card.Title>
                 <Card.Text>
-                    The {item} is ${price} at {site}
+                    {item.replace(/^./, item[0].toUpperCase())}: <b className="text-dark">${price}</b> at {site}
                 </Card.Text>
                 {/* Dont know how to put link into here yet im sure its simple */}
-                <Button variant="outline-primary">To page</Button>
             </Card.Body>
         </Card>
     );
